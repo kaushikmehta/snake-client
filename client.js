bf93c7a7666b122fbe3da2c034b7820a13416b17
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-07-01 18:50:08 +0000

    tested moves

--- client.js
+++ client.js
@@ -1,7 +1,9 @@
 const net = require('net');
+const { stderr, stdin } = require('process');
 
 const connect = function() {
   const conn = net.createConnection({ 
+    // host: '135.23.222.148',
     host: 'localhost',
     port: 50541
   });
@@ -10,7 +12,11 @@ const connect = function() {
   conn.on('connect', (server) => {
     console.log("Successfully connected to game");
     // console.log(server)
-    conn.write("Name: KM")
+    conn.write("Name: KM");
+    
+    // stdin.on('data', function (data) {
+
+    // });    
   })
   
   conn.on('data', (data) => {
