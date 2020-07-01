d4d6a927eafd179cc039fb46bcd09e5c0b184b7c
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-07-01 19:23:02 +0000

    updated input to pass movement commands

--- input.js
+++ input.js
@@ -1,18 +1,32 @@
 const { stdin } = require('process');
 
-const setupInput = function () {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
-  // const handleUserInput = () => {
-    stdin.on("data", key => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    })
-  // }
+  const handleUserInput = () => {
+  stdin.on("data", key => {
+    if (key === '\u0003') {
+      process.exit();
+    } else if (key === 'w') {
+      connection.write("Move: up");
+    } else if (key === 'a') {
+      connection.write("Move: left");
+    } else if (key === 's') {
+      connection.write("Move: down");
+    } else if (key === 'd') {
+      connection.write("Move: right");
+    }
+
+  })
+  }
+  handleUserInput();
 
   return stdin;
 }
--- play.js
+++ play.js
@@ -1,6 +1,6 @@
-const {connect} = require('./client');
+const { connect } = require('./client');
 const { setupInput } = require('./input');
 
-setupInput();
 console.log('Connecting ...');
-connect();
+
+setupInput(connect());
