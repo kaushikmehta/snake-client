9d7e01c2f3d447865ee55d9d8593827432d57305
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-07-01 19:12:36 +0000

    setup input module

---
+++ input.js
@@ -0,0 +1,26 @@
+const { stdin } = require('process');
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  // const handleUserInput = () => {
+    stdin.on("data", key => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    })
+  // }
+
+  return stdin;
+}
+
+
+
+
+// handleUserInput();
+
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,27 +1,6 @@
 const {connect} = require('./client');
-const { stdin } = require('process');
-/**
- * Establishes connection with the game server
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
+const { setupInput } = require('./input');
 
-
-const handleUserInput = () => {
-  stdin.on("data", key => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
-}
 setupInput();
-
-handleUserInput();
-
 console.log('Connecting ...');
 connect();
