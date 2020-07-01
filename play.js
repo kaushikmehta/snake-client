8bb7565d9c7ee6e5463183536e2128e214f2239e
Kaushik Mehta <kaushikmehta30@gmail.com>
2020-07-01 18:58:43 +0000

    handled user input

--- play.js
+++ play.js
@@ -1,9 +1,27 @@
 const {connect} = require('./client');
+const { stdin } = require('process');
 /**
  * Establishes connection with the game server
  */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
 
 
+const handleUserInput = () => {
+  stdin.on("data", key => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+}
+setupInput();
+
+handleUserInput();
 
 console.log('Connecting ...');
 connect();
